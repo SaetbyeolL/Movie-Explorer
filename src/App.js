693b128e38af040c386cc63f54293aa9b21f543a
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-03-10 23:28:15 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />}/>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+// Switch: look for Route(URL). when it find Route, render component
+// switch render only one route at a time
+
+// Link: moves the user to another page without refreshing the browser.
+
